6365964961e128ca792e7178091aeabb782129e4
Jared Luedtke <jared7223@hotmail.com>
2017-06-16 10:03:29 -0700

    add Save method for Band.cs

--- Objects/Band.cs
+++ Objects/Band.cs
@@ -15,6 +15,31 @@ namespace BandTracker
       Name = name;
     }
 
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO bands (name) OUTPUT INSERTED.id VALUES (@Name);", conn);
+
+      cmd.Parameters.Add(new SqlParameter("@Name", this.Name));
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this.Id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
     public static List<Band> GetAll()
     {
       List<Band> allBands = new List<Band>();
