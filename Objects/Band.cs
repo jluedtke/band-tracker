8b328d64d96f32160c81e3526124d88b83a7e923
Jared Luedtke <jared7223@hotmail.com>
2017-06-16 09:58:10 -0700

    add Equals Override method for Band.cs

--- Objects/Band.cs
+++ Objects/Band.cs
@@ -14,5 +14,31 @@ namespace BandTracker
       Id = id;
       Name = name;
     }
+
+    public override bool Equals(System.Object otherBand)
+    {
+      if (!(otherBand is Band))
+      {
+        return false;
+      }
+      else
+      {
+        Band newBand = (Band) otherBand;
+        bool idEquals = (this.Id == newBand.Id);
+        bool nameEquals = (this.Name == newBand.Name);
+        return (idEquals && nameEquals);
+      }
+    }
+
+
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM bands;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
   }
 }
