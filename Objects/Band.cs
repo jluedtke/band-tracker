486951f809ff23015635ae43d69b408626aea8be
Jared Luedtke <jared7223@hotmail.com>
2017-06-16 09:59:20 -0700

    add GetAll method for Band.cs

--- Objects/Band.cs
+++ Objects/Band.cs
@@ -15,6 +15,32 @@ namespace BandTracker
       Name = name;
     }
 
+    public static List<Band> GetAll()
+    {
+      List<Band> allBands = new List<Band>();
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+      while (rdr.Read())
+      {
+        int bandId = rdr.GetInt32(0);
+        string bandName = rdr.GetString(1);
+        Band newBand = new Band(bandName, bandId);
+        allBands.Add(newBand);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        rdr.Close();
+      }
+      return allBands;
+    }
+
     public override bool Equals(System.Object otherBand)
     {
       if (!(otherBand is Band))
